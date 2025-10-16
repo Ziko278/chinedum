0d296be3e7fcdd4c12282595aba8d7ec1befd607
ziko278 <odekeziko@gmail.com>
2025-10-16 18:41:37 +0100

    updated

--- user/models.py
+++ user/models.py
@@ -5,6 +5,6 @@ from staff.models import StaffModel
 
 # Create your models here.
 class UserRoleModel(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='account')
     staff = models.ForeignKey(StaffModel, on_delete=models.CASCADE, null=True, blank=True)
     role = models.CharField(max_length=100)
